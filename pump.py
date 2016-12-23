bf7f64468ec2b676cde0dc92531683f0e0ffaad9
stuartsmiley <stuart.a.smiley@gmail.com>
2016-12-23 19:47:10 +0000

    Added periodic configuration reloading as needed and proper logging

--- pump.py
+++ pump.py
@@ -6,7 +6,11 @@ import os
 import signal
 import re
 import time
+import logging
 
+CFG_NAME = 'pump.cfg'
+CFG_INCREMENT = 60 * 5
+CONFIG_STAMP = os.stat(CFG_NAME).st_mtime
 WAIT_INCREMENT = 0
 RUN_INCREMENT = 0
 DATA_FILE = os.path.join("/", "home", "pi", "whatever")
@@ -28,12 +32,12 @@ def initialize():
 def check_data_file():
     global LAST_RUN_DATETIME
     if os.path.isfile(DATA_FILE):
-        print("Found expected data file")
+        logger.info("Found expected data file")
         last = last_line()
         LAST_RUN_DATETIME = last_run(last)
     else:
-        print("Did NOT Find expected data file " + DATA_FILE)
-        print("Seeding " + DATA_FILE)
+        logger.info("Did NOT Find expected data file " + DATA_FILE)
+        logger.info("Seeding " + DATA_FILE)
         seconds_in_day = 60 * 60 * 24
         seed_delta = datetime.timedelta(seconds=seconds_in_day)
         LAST_RUN_DATETIME = datetime.datetime.now() - seed_delta
@@ -49,21 +53,21 @@ def last_line():
 def roll():
     last_run_date = LAST_RUN_DATETIME.date()
     today = datetime.date.today()
-    print("Comparing " + str(last_run_date) + " vs " + str(today))
+    logger.debug("Comparing " + str(last_run_date) + " vs " + str(today))
     if last_run_date != today:
         os.rename(DATA_FILE, DATA_FILE + "." + str(last_run_date)) 
-        print("rolled " + DATA_FILE)
+        logger.info("rolled " + DATA_FILE)
 
 def last_run(last_log_message):
     p =  re.compile('o.* (20.*)')
     m = p.match(last_log_message)
     last_run = m.group(1)
-    print('Last run at ' + last_run)
+    logger.debug('Last run at ' + last_run)
     last_run_date = datetime.datetime.strptime(last_run, "%Y-%m-%d %H:%M:%S.%f")
     return last_run_date
 
 def time_to_next(last_log_message):
-    print('Last message: ' + last_log_message)
+    logger.debug('Last message: ' + last_log_message)
     last_run_date = last_run(last_log_message)
     now = datetime.datetime.now()
     current_delta = now - last_run_date
@@ -75,8 +79,9 @@ def time_to_next(last_log_message):
 
 def ask_exit(signame):
     update_data_file('off and shutdown ' + str(datetime.datetime.now()) + '\n')
-    print("got signal %s: exit" % signame)
+    logger.info("got signal %s: exit" % signame)
     loop.stop()
+    cfg_loop.stop()
 
 
 def execute(status, loop):
@@ -101,6 +106,15 @@ def update_data_file(line):
     with open(DATA_FILE, 'a') as f:
         f.write(line)
 
+def check_configuration(loop):
+    global CONFIG_STAMP
+    current_stamp = os.stat(CFG_NAME).st_mtime
+    if CONFIG_STAMP != current_stamp:
+        logger.info("Configuration changed: re-initializing")
+        initialize()
+        CONFIG_STAMP = current_stamp
+    loop.call_later(CFG_INCREMENT, check_configuration, loop)
+
 initialize()
 if RUN_INCREMENT == 0:
     raise SystemExit
@@ -111,20 +125,32 @@ if RUN_INCREMENT == 0:
 time.sleep(INITIAL_SLEEP_TIME)
 
 loop = asyncio.get_event_loop()
+cfg_loop = asyncio.get_event_loop()
 for signame in ('SIGINT', 'SIGTERM'):
     loop.add_signal_handler(getattr(signal, signame),
                             functools.partial(ask_exit, signame))
+    cfg_loop.add_signal_handler(getattr(signal, signame),
+                            functools.partial(ask_exit, signame))
+
+logger = logging.getLogger('pump_application')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('/home/pi/pump.log')
+formatter = logging.Formatter('%(asctime)s - %(threadName)s %(levelname)s : %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 check_data_file()
 last = last_line()
-print(last)
+logger.debug(last)
 next = time_to_next(last)
-print("Next run starting in seconds: " + str(next))
-print("Event loop running forever, press Ctrl+C to interrupt.")
-print("pid %s: send SIGINT or SIGTERM to exit." % os.getpid())
+logger.debug("Next run starting in seconds: " + str(next))
+logger.debug("Event loop running forever, press Ctrl+C to interrupt.")
+logger.debug("pid %s: send SIGINT or SIGTERM to exit." % os.getpid())
 loop.call_later(next, execute, False, loop) 
-
+cfg_loop.call_later(CFG_INCREMENT, check_configuration, cfg_loop)
 try:
     loop.run_forever()
+    cfg_loop.run_forever()
 finally:
     loop.close()
+    cfg_loop.close()
