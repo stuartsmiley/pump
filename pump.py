0b84796e7e7dc58b78324451cb3a6f4c9eca5711
stuartsmiley <stuart.a.smiley@gmail.com>
2016-12-23 18:18:38 +0000

    Added rolling of the datafile

--- pump.py
+++ pump.py
@@ -11,6 +11,7 @@ WAIT_INCREMENT = 0
 RUN_INCREMENT = 0
 DATA_FILE = os.path.join("/", "home", "pi", "whatever")
 INITIAL_SLEEP_TIME = 0
+LAST_RUN_DATETIME = datetime.datetime.now()
 
 def initialize():
     config = configparser.ConfigParser()
@@ -25,15 +26,18 @@ def initialize():
     DATA_FILE = config['DEFAULT']['DataFile']
 
 def check_data_file():
+    global LAST_RUN_DATETIME
     if os.path.isfile(DATA_FILE):
         print("Found expected data file")
+        last = last_line()
+        LAST_RUN_DATETIME = last_run(last)
     else:
         print("Did NOT Find expected data file " + DATA_FILE)
         print("Seeding " + DATA_FILE)
         seconds_in_day = 60 * 60 * 24
         seed_delta = datetime.timedelta(seconds=seconds_in_day)
-        seed_date = datetime.datetime.now() - seed_delta
-        update_data_file('off ' + str(seed_date) + '\n')
+        LAST_RUN_DATETIME = datetime.datetime.now() - seed_delta
+        update_data_file('off ' + str(LAST_RUN_DATETIME) + '\n')
 
 def last_line():
     last = None
@@ -42,12 +46,25 @@ def last_line():
             pass
     return last
 
-def time_to_next(last_log_message):
+def roll():
+    last_run_date = LAST_RUN_DATETIME.date()
+    today = datetime.date.today()
+    print("Comparing " + str(last_run_date) + " vs " + str(today))
+    if last_run_date != today:
+        os.rename(DATA_FILE, DATA_FILE + "." + str(last_run_date)) 
+        print("rolled " + DATA_FILE)
+
+def last_run(last_log_message):
     p =  re.compile('o.* (20.*)')
     m = p.match(last_log_message)
     last_run = m.group(1)
     print('Last run at ' + last_run)
     last_run_date = datetime.datetime.strptime(last_run, "%Y-%m-%d %H:%M:%S.%f")
+    return last_run_date
+
+def time_to_next(last_log_message):
+    print('Last message: ' + last_log_message)
+    last_run_date = last_run(last_log_message)
     now = datetime.datetime.now()
     current_delta = now - last_run_date
     next_run = 0
@@ -56,7 +73,6 @@ def time_to_next(last_log_message):
         next_run = current_delta.total_seconds()        
     return next_run
 
-
 def ask_exit(signame):
     update_data_file('off and shutdown ' + str(datetime.datetime.now()) + '\n')
     print("got signal %s: exit" % signame)
@@ -64,15 +80,21 @@ def ask_exit(signame):
 
 
 def execute(status, loop):
+    roll()
+    global LAST_RUN_DATETIME
     ssr = 'off'
     increment = WAIT_INCREMENT
     if status:
+        # turn pump off
         status = False
     else:
+        # turn pump on
         ssr = 'on'
         increment = RUN_INCREMENT
         status = True
-    update_data_file(ssr + ' ' + str(datetime.datetime.now()) + '\n')
+    now = datetime.datetime.now()
+    update_data_file(ssr + ' ' + str(now) + '\n')
+    LAST_RUN_DATETIME = now
     loop.call_later(increment, execute, status, loop)
 
 def update_data_file(line):
